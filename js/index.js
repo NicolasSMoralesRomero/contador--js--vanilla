1a7a34b1d633575b12e56a9843b092f8373abf56
Nicolas Morales <nicosmr56@gmail.com>
2024-06-10 14:23:21 -0300

    inicio-proyecto

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let suma = document.querySelector("#sumar");
+let resta = document.querySelector("#restar")
+let resultado = document.querySelector("#resultado")
+let reset = document.querySelector("#reset")
+
+function sumar(){
+    console.log("hola")
+}
+
+suma.addEventListener("click", sumar)
