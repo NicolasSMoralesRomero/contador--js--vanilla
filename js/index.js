98af0268d3d9a1f481165e8add936192372f5fac
Nicolas Morales <nicosmr56@gmail.com>
2024-06-12 14:15:03 -0300

    cambios-logica

--- js/index.js
+++ js/index.js
@@ -4,7 +4,35 @@ let resultado = document.querySelector("#resultado")
 let reset = document.querySelector("#reset")
 
 function sumar(){
-    console.log("hola")
+    const numeroActual = parseInt(resultado.innerHTML);
+    resultado.innerHTML = numeroActual + 1;
 }
 
-suma.addEventListener("click", sumar)
+function restar(){
+    const numeroActual = parseInt(resultado.innerHTML);
+    if(numeroActual <= 0){
+        return
+    }else{
+        resultado.innerHTML = numeroActual - 1;
+    }
+    
+}
+
+function limpiar(){
+    resultado.innerHTML = 0;
+}
+
+suma.addEventListener('click', (event)=>{
+    event.preventDefault();
+    sumar();
+})
+
+resta.addEventListener('click', (event) =>{
+    event.preventDefault();
+    restar();
+})
+
+reset.addEventListener('click', (event) =>{
+    event.preventDefault();
+    limpiar();
+})
